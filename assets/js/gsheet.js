2b22efba3d2c696d3e374985e4a9a4a716589e41
nanansm <smnanan@gmail.com>
2023-09-18 11:50:43 +0700

    commit

--- assets/js/gsheet.js
+++ assets/js/gsheet.js
@@ -1,6 +1,3 @@
-
-
-
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzETkUHS-sFdfoEJOVaHQJjofaTOnG_EonkX1in4A9FtFiHAD9razP2EXUuxjY969GF/exec'
 
 const form = document.forms['lp-data']
@@ -12,24 +9,20 @@ form.addEventListener('submit', e => {
   //.then(() => { window.location.reload(); })
   .catch(error => console.error('Masukan Data Kamu dulu yah!', error.message)) })
 
-function towa()
-{
-   window.location.href="https://api.whatsapp.com/send?phone=6281122332060&text=Halo%20Laidlunos,%20aku%20mau%20claim%20Discount%20Produk%20Simple%20Flek%20Bundle%20yah!";
-    
+
+//function towa() {
+  // window.location.href="https://api.whatsapp.com/send?phone=6281122332060&text=Halo%20Laidlunos,%20aku%20mau%20claim%20Discount%20Produk%20Simple%20Flek%20Bundle%20yah!"; }
+
+  
+   function towa() {
+    var fname = document.forms['lp-data']['fname'].value;
+    var wnumber = document.forms['lp-data']['wnumber'].value;
+
+    if (fname === "" || wnumber === "") {
+        alert("Mohon isi Nama & No Whatsapp Kamu.");
+    } else {
+        // Semua field telah diisi, maka arahkan pengguna ke WhatsApp
+        var whatsappLink = "https://api.whatsapp.com/send?phone=6281122332060&text=Halo%20Laidlunos,%20aku%20mau%20claim%20Discount%20Produk%20Simple%20Flek%20Bundle%20yah!";
+        window.location.href = whatsappLink;
+    }
 }
-
-
-//function gotowhatsapp() {
-
-  //  var name = document.getElementById("name").value;
-  //  var phone = document.getElementById("phone").value;
-  //  var email = document.getElementById("email").value;
-  //  var service = document.getElementById("service").value;
-
-  //  var url = "https://wa.me/628112131496?text="
-  //     + "Name: " + name + "%0a"
-  //    + "Phone: " + phone + "%0a"
-  //   + "Email: " + email + "%0a"
-  //  + "Service: " + service;
-
-    // window.open(url, '_blank').focus(); }
